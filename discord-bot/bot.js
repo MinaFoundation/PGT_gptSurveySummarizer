804246bf2c5545ceffae59ffddf2f9d756ef0b7b
Evan Shapiro <evan.a.shapiro@gmail.com>
2024-04-02 23:02:50 +0900

    mostly implemented

--- discord-bot/bot.js
+++ discord-bot/bot.js
@@ -1,78 +1,278 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v10');
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { createClient } = require('redis');
-const openai = require('openai');
+import 'dotenv/config'
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+import { Client, GatewayIntentBits, TextInputBuilder, TextInputStyle } from 'discord.js';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v10';
 
-const redisClient = createClient({ url: process.env.REDIS_URL });
-redisClient.connect(); // TODO maybe put this in an async function and block til ready
+import { ActionRowBuilder, ModalBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } from '@discordjs/builders';
+import { createClient } from 'redis';
+import openai from 'openai';
 
-client.once('ready', () => {
-    console.log('Ready!');
-});
+(async() => {
 
-const commands = [
-    new SlashCommandBuilder().setName('createsurvey').setDescription('Create a new survey')
-        .addStringOption(option => option.setName('name').setDescription('The name of the survey').setRequired(true)),
-    new SlashCommandBuilder().setName('listsurveys').setDescription('List all available surveys'),
-    new SlashCommandBuilder().setName('respond').setDescription('Respond to a survey')
-        .addStringOption(option => option.setName('name').setDescription('The name of the survey').setRequired(true))
-        .addStringOption(option => option.setName('response').setDescription('Your response').setRequired(true)),
-    new SlashCommandBuilder().setName('summary').setDescription('Get the summary of a survey')
-        .addStringOption(option => option.setName('name').setDescription('The name of the survey').setRequired(true)),
-].map(command => command.toJSON());
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+  const redisClient = createClient({ url: process.env.REDIS_URL });
+  await redisClient.connect();
 
-(async () => {
-    try {
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-            { body: commands },
-        );
+  const command = 
+    new SlashCommandBuilder()
+    .setName('gptsurvey')
+    .setDescription('create, respond to, and view gpt-powered natural language surveys')
+    .addSubcommand(sc => 
+      sc
+        .setName('create')
+        .setDescription('create a new survey')
+    )
+    .addSubcommand(sc => 
+      sc
+        .setName('respond')
+        .setDescription('respond to a survey')
+        .addStringOption(option =>
+          option.setName('survey')
+            .setDescription('survey name')
+            .setAutocomplete(true)
+            .setRequired(true))
+    )
+    .addSubcommand(sc => 
+      sc
+        .setName('view')
+        .setDescription('view the summary and responses for a survey')
+        .addStringOption(option =>
+          option.setName('survey')
+            .setDescription('survey name')
+            .setAutocomplete(true)
+            .setRequired(true))
+    )
 
-        console.log('Successfully registered commands.');
-    } catch (error) {
-        console.error(error);
+  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+  try {
+      await rest.put(
+          Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+          { body: [ command.toJSON() ] },
+      );
+
+      console.log('Successfully registered commands.');
+  } catch (error) {
+      console.error(error);
+  }
+
+  client.on('interactionCreate', async interaction => {
+    const { user } = interaction;
+
+    // TODO check that these are unique
+    // TODO check that users can't change this, if so consider using ids in maps
+    const username = user.username; 
+
+    // ------------------------------------------------
+
+    if (interaction.isChatInputCommand()) {
+      const { commandName, options } = interaction;
+
+      const subcommand = interaction.options.getSubcommand()
+
+      // ------------------------------------------------
+      if (subcommand == 'create') {
+        const modal = new ModalBuilder()
+          .setCustomId('createModal')
+          .setTitle('Create Survey');
+
+        const titleInput = new TextInputBuilder()
+          .setCustomId('titleInput')
+          .setLabel("What is your survey title?")
+          .setStyle(TextInputStyle.Short)
+          .setRequired(true);
+
+        const descriptionInput = new TextInputBuilder()
+          .setCustomId('descriptionInput')
+          .setLabel("Write a short description for your survey")
+          .setStyle(TextInputStyle.Paragraph);
+
+        const firstActionRow = new ActionRowBuilder().addComponents(titleInput);
+        const secondActionRow = new ActionRowBuilder().addComponents(descriptionInput);
+        modal.addComponents(firstActionRow, secondActionRow);
+
+        await interaction.showModal(modal);
+
+      // ------------------------------------------------
+      } else if (subcommand == 'respond') {
+        const surveyName = options.getString('survey');
+
+        const surveyDescription = await redisClient.get(`survey:${surveyName}:description`);
+
+        const modal = new ModalBuilder()
+          .setCustomId('respondModal-' + surveyName)
+          .setTitle('Respond to Survey');
+
+        const responseInput = new TextInputBuilder()
+          .setCustomId('responseInput')
+          .setLabel(`${surveyName} | ${surveyDescription}`)
+          .setStyle(TextInputStyle.Paragraph)
+          .setRequired(true);
+
+        const actionRow = new ActionRowBuilder().addComponents(responseInput);
+        modal.addComponents(actionRow);
+
+        await interaction.showModal(modal);
+
+      // ------------------------------------------------
+      } else if (subcommand == 'view') {
+        const surveyName = options.getString('survey');
+        const summaryJSON = await redisClient.get(`survey:${surveyName}:summary`);
+        const description = await redisClient.get(`survey:${surveyName}:description`);
+        const creator = await redisClient.get(`survey:${surveyName}:username`);
+        const responses = await redisClient.hGetAll(`survey:${surveyName}:responses`);
+        const summary = JSON.parse(summaryJSON);
+
+        const summarizedResponses = []
+
+        await interaction.reply('Please see the following latest survey summary:');
+
+        const channel = client.channels.cache.get(interaction.channelId)
+
+        let msg = `# ${surveyName}\n`
+        msg += `${description}\n`
+        msg += `created by ${creator}\n`
+        msg += `----------------------\n`
+        await channel.send(msg);
+        msg = ``;
+        for (const topic of summary.taxonomy) {
+          msg += `## ${topic.topicName}\n`
+          msg += `${topic.topicShortDescription}\n`
+          // TODO post # of responses and % out of total responses
+          for (const subtopic of topic.subtopics) {
+            msg += `## ${subtopic.subtopicName}\n`
+            msg += `${subtopic.subtopicShortDescription}\n`
+            // TODO post # of responses and % out of topic
+            const subtopicMessage = await channel.send(msg);
+            msg = ``;
+            if (subtopic.responses.length > 0) {
+              const thread = await subtopicMessage.startThread({
+                name: `${subtopic.subtopicName} responses`,
+                autoArchiveDuration: 60,
+                reason: 'Thread for responses'
+              });
+              for (const response of subtopic.responses) {
+                await thread.send(response.username + ' said "' + response.response + '"');
+                summarizedResponses.push(response);
+              }
+              await thread.setLocked(true);
+            }
+          }
+        }
+        if (summary.unmatchedResponses.length > 0) {
+          msg += `## Unmatched Responses\n`
+          const unmatchedMessage = await channel.send(msg);
+          msg = ``;
+          const thread = await unmatchedMessage.startThread({
+            name: `Unmatched responses`,
+            autoArchiveDuration: 60,
+            reason: 'Thread for responses'
+          });
+          for (const response of summary.unmatchedResponses) {
+            for (const response of subtopic.responses) {
+              await thread.send(response.username + ' said "' + response.response + '"');
+              summarizedResponses.push(response);
+            }
+          }
+        }
+
+        const unsummarizedResponses = []; // TODO
+        if (unsummarizedResponses.length > 0) {
+          msg += `## Responses Not Yet Categorized\n`
+          // TODO post when update will happen
+          const unsummarizedMessage = await channel.send(msg);
+          msg = ``;
+          const thread = await unsummarizedMessage.startThread({
+            name: `Unsummarized responses`,
+            autoArchiveDuration: 60,
+            reason: 'Thread for responses'
+          });
+          for (const response of unsummarizedResponses) {
+            await thread.send(response.username + ' said "' + response.response + '"');
+          }
+        }
+        
+        if (msg.length > 0) {
+          await channel.send(msg);
+        }
+
+      // ------------------------------------------------
+      } else {
+        console.error('unknown subcommand');
+      }
+
+    // ------------------------------------------------
+    } else if (interaction.isModalSubmit()) {
+
+      // ------------------------------------------------
+    	if (interaction.customId === 'createModal') {
+        const title = interaction.fields.getTextInputValue('titleInput');
+        const description = interaction.fields.getTextInputValue('descriptionInput');
+        if (await redisClient.sIsMember('surveys', surveyName)) {
+          await interaction.reply({ content: 'A survey with that name already exists' });
+        } else {
+          createSurvey(
+            redisClient, 
+            title, 
+            description,
+            username
+          );
+          await interaction.reply({ content: 'Your Survey was created successfully!' });
+        }
+
+      // ------------------------------------------------
+      } else if (interaction.customId.startsWith('respondModal')) {
+        const surveyName = interaction.customId.split('-').slice(1).join('-');
+        const response = interaction.fields.getTextInputValue('responseInput')
+        const hadResponse = await redisClient.hExists(`survey:${surveyName}:responses`, username)
+        await respond(redisClient, surveyName, username, response)
+        if (hadResponse) {
+          await interaction.reply({ content: 'Your Response was updated successfully!' });
+        } else {
+          await interaction.reply({ content: 'Your Response was added successfully!' });
+        }
+      }
+    // ------------------------------------------------
+    } else if (interaction.isAutocomplete()) {
+      const surveys = await redisClient.sMembers('surveys');
+
+      const focusedValue = interaction.options.getFocused();
+      const choices = surveys;
+      const filtered = choices.filter(choice => choice.startsWith(focusedValue));
+
+      await interaction.respond(
+        filtered.map(choice => ({ name: choice, value: choice })),
+      );
     }
+  });
+
+  client.login(process.env.DISCORD_TOKEN);
+
+  //await runTest(redisClient);
 })();
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+const runTest = async (redisClient) => {
+  await redisClient.flushAll();
+  await createSurvey(redisClient, 'test-survey', 'test-description', 'evan');
+  await respond(redisClient, 'test-survey', 'evan', 'comment1');
+  await respond(redisClient, 'test-survey', 'bob', 'comment2');
+}
 
-    const { commandName, options } = interaction;
-    const surveyName = options.getString('name');
+const createSurvey = async (redisClient, surveyName, description, username) => {
+  await redisClient.sAdd('surveys', surveyName);
+  const initialSummaryJSON = JSON.stringify({});
+  await redisClient.set(`survey:${surveyName}:summary`, initialSummaryJSON);
+  await redisClient.set(`survey:${surveyName}:title`, surveyName);
+  await redisClient.set(`survey:${surveyName}:description`, description);
+  await redisClient.set(`survey:${surveyName}:username`, username);
+  await redisClient.set(`survey:${surveyName}:last-edit-time`, Date.now());
+  await redisClient.set(`survey:${surveyName}:last-summary-time`, Date.now());
+}
 
-    switch (commandName) {
-        case 'createsurvey':
-            await redisClient.sAdd('surveys', surveyName);
-            await interaction.reply(`Survey ${surveyName} created.`);
-            const initialSummaryJSON = JSON.stringify({});
-            await redisClient.set('survey:${surveyName}:summary', initialSummaryJSON);
-            break;
-        case 'listsurveys':
-            const surveys = await redisClient.sMembers('surveys');
-            await interaction.reply(`Available surveys: ${surveys.join(', ')}`);
-            break;
-        case 'respond':
-            const response = options.getString('response');
-            // TODO make this a map with the discord name so users can update their previous responses
-            await redisClient.rPush(`survey:${surveyName}:responses`, response);
-            await interaction.reply(`Response to ${surveyName} recorded.`);
-            // TODO write a "last edit time" to redis, so that if the summarizer is down it can still look at it
-            await redisClient.publish('survey-refresh', surveyName);
-            break;
-        case 'summary':
-            const summaryJSON = await redisClient.get('survey:${surveyName}:summary');
-            // TODO format this
-            await interaction.reply(`Summary for ${surveyName}: ${summaryJSON}`);
-            break;
-    }
-});
-
-client.login(process.env.DISCORD_TOKEN);
-
+const respond = async (redisClient, surveyName, username, response) => {
+  await redisClient.hSet(`survey:${surveyName}:responses`, username, response);
+  await redisClient.set(`survey:${surveyName}:last-edit-time`, Date.now());
+  await redisClient.publish('survey-refresh', surveyName);
+}
--- discord-bot/summarizer.js
+++ discord-bot/summarizer.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
-const { createClient } = require('redis');
+import 'dotenv/config'
+import OpenAI from "openai";
+import { createClient } from 'redis';
 
 const subscribeRedisClient = createClient({ url: process.env.REDIS_URL });
 subscribeRedisClient.on('error', err => console.log('Redis Client Error', err));
@@ -7,18 +8,201 @@ subscribeRedisClient.on('error', err => console.log('Redis Client Error', err));
 const redisClient = createClient({ url: process.env.REDIS_URL });
 redisClient.on('error', err => console.log('Redis Client Error', err));
 
-// TODO look at all the last edit times vs all the last summary times for all surveys on startup, to catch any summaries that should be made since the summarizer wasn't running
+const summarizeFrequency = process.env.SUMMARIZE_FREQUENCY;
 
-(async () => {
+// ==========================================================================================
+
+const main = async () => {
   await subscribeRedisClient.connect();
   await redisClient.connect();
 
-  // TODO run summarizing across all surveys once per hour (publish/ subscribe may actually not be needed at that point)
+  while (true) {
+    console.log('1');
+    await checkUpdateSurveys(redisClient);
+    await new Promise((r) => setTimeout(r, 30*1000));
+  }
 
-  await subscribeRedisClient.subscribe('survey-refresh', async (surveyName) => {
-    // TODO actually call gpt
-    console.log('create summary for', surveyName);
-    await redisClient.set('survey:${surveyName}:summary', 'summary for ' + surveyName)
+  // NOTE this updates every time a new response shows up; disabled for now
+  //await subscribeRedisClient.subscribe('survey-refresh', async (surveyName) => {
+  //  updateSurvey(surveyName);
+  //});
+};
+
+const checkUpdateSurveys = async (redisClient) => {
+  const surveys = await redisClient.sMembers('surveys');
+  for (let surveyName of surveys){
+    let lastEditTime = await redisClient.get(`survey:${surveyName}:last-edit-time`);
+    let lastSummaryTime = await redisClient.get(`survey:${surveyName}:last-summary-time`);
+
+    lastEditTime = parseInt(lastEditTime);
+    lastSummaryTime = parseInt(lastSummaryTime);
+
+    const lastSummaryUpdateEpoch = Math.floor(lastSummaryTime/(summarizeFrequency*1000));
+    const currentEpoch = Math.floor(Date.now()/(summarizeFrequency*1000));
+
+    console.log(lastEditTime, lastSummaryTime, lastSummaryUpdateEpoch, currentEpoch);
+
+    if (lastEditTime != null) {
+      if (lastSummaryTime == null) {
+        await updateSurvey(redisClient, surveyName);
+      } else {
+        if (lastEditTime >= lastSummaryTime && lastSummaryUpdateEpoch != currentEpoch) {
+          if (lastSummaryUpdateEpoch != currentEpoch) {
+            await updateSurvey(redisClient, surveyName);
+          }
+        }
+      }
+    }
+  }
+
+}
+
+// ==========================================================================================
+
+const updateSurvey = async (
+  redisClient,
+  surveyName
+) => {
+  const responses = await redisClient.hGetAll(`survey:${surveyName}:responses`);
+  console.log('creating summary for', surveyName);
+  console.log('responses', responses)
+
+  const apikey = process.env.OPENAI_API_KEY;
+
+  const { taxonomy } = await gpt(
+    apikey,
+    systemMessage(),
+    clusteringPrompt(JSON.stringify(Object.values(responses))),
+  );
+
+  const batchSize = 10;
+
+  const unmatchedResponses = [];
+
+  for (let i = 0; i < Object.keys(responses).length; i += batchSize) {
+    const batch = Object.entries(responses).slice(i, i + batchSize);
+    await Promise.all(
+      batch.map(async ([ username, response ]) => {
+        const assignment = await gpt(
+          apikey,
+          systemMessage(),
+          assignmentPrompt(JSON.stringify(taxonomy), response)
+        );
+        insertResponse(
+          taxonomy,
+          assignment,
+          { response, username },
+          unmatchedResponses,
+        );
+      })
+    );
+  }
+
+  const summary = {
+    taxonomy,
+    unmatchedResponses,
+  }
+
+  console.log(JSON.stringify(summary, null, 2));
+
+  await redisClient.set(`survey:${surveyName}:summary`, JSON.stringify(summary));
+  await redisClient.set(`survey:${surveyName}:last-summary-time`, Date.now());
+}
+
+// ==========================================================================================
+
+const gpt = async (
+  apikey,
+  system,
+  user,
+) => {
+  const openai = new OpenAI({ apikey });
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: system },
+      { role: "user", content: user },
+    ],
+    model: "gpt-3.5-turbo",
+    response_format: { type: "json_object" },
   });
+  const { finish_reason, message } = completion.choices[0];
+  const result = JSON.parse(message.content);
+  return result;
+}
 
-})();
+export const systemMessage = () => `
+You are a professional research assistant. You have helped run many public consultations, 
+surveys and citizen assemblies. You have good instincts when it comes to extracting interesting insights. 
+You are familiar with public consultation tools like Pol.is and you understand the benefits 
+for working with very clear, concise claims that other people would be able to vote on.
+`;
+
+export const clusteringPrompt = (responses) => `
+I will give you a list of responses.
+I want you to propose a way to break down the information contained in these responses into topics and subtopics of interest. 
+Keep the topic and subtopic names very concise and use the short description to explain what the topic is about. Each topic must have at least one subtopic.
+
+Return a JSON object of the form {
+  "taxonomy": [
+    {
+      "topicName": string, 
+      "topicShortDescription": string,
+      "subtopics": [
+        {
+          "subtopicName": string,  
+          "subtopicShortDescription": string, 
+        },
+        ...
+      ]
+    }, 
+    ... 
+  ]
+}
+
+Now here is the list of responses:
+${responses}
+`;
+
+export const assignmentPrompt = (
+  taxonomy,
+  response,
+) => `
+I'm going to give you a response made by a participant and a list of topics and subtopics which have already been extracted.  
+I want you to assign each response to the best matching topic and subtopic in the taxonomy. The topic must be a member of the taxonomy, and the subtopic must be a member of the topic.
+
+Return a JSON object of the form {
+  "topicName": string // from the given list of topics
+  "subtopicName": string // from the list of subtopics
+}
+
+Now here is the list of topics/subtopics: 
+taxonomy: ${taxonomy}
+
+And then here is the response:
+${response} 
+`;
+
+function insertResponse(taxonomy, assignment, response, unmatchedResponses) {
+  const { topicName, subtopicName } = assignment;
+  const matchedTopic = taxonomy.find((topic) => topic.topicName === topicName);
+  if (!matchedTopic) {
+    console.log("Topic missmatch, skipping response " + JSON.stringify(response));
+    unmatchedResponses.push(response);
+    return;
+  }
+  const subtopic = matchedTopic.subtopics.find(
+    (subtopic) => subtopic.subtopicName === subtopicName
+  );
+  if (!subtopic) {
+    console.log("Subtopic missmatch, skipping response " + JSON.stringify(response));
+    unmatchedResponses.push(response);
+    return;
+  }
+  if (!subtopic.responses) {
+    subtopic.responses = [];
+  }
+  subtopic.responses.push(response);
+}
+
+main()
