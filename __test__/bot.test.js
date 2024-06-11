046cf77fc0aeba27f7c3dcced20481093890c7bb
miors <mior.sufian@googlemail.com>
2024-06-11 09:03:53 +0100

    unit testing POC

---
+++ __test__/bot.test.js
@@ -0,0 +1,18 @@
+const { interactionCreateHandler } = require("../src/handlers/bot");
+
+describe("interactionCreateHandler", () => {
+  const interaction = {
+    user: { username: "mior8667" },
+    isChatInputCommand: jest.fn(),
+    isButton: jest.fn(),
+    isModalSubmit: jest.fn(),
+    isAutocomplete: jest.fn(),
+  };
+  it("should call interaction create handler", async () => {
+    interactionCreateHandler(interaction);
+    expect(interaction.isChatInputCommand).toHaveBeenCalledWith();
+    expect(interaction.isButton).toHaveBeenCalledWith();
+    expect(interaction.isModalSubmit).toHaveBeenCalledWith();
+    expect(interaction.isAutocomplete).toHaveBeenCalledWith();
+  });
+});
