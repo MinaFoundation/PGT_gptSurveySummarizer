70741c1c80f1cd0c6228a8eb9d7cce0b34d27c26
berkingurcan <berkingurcan@yandex.com>
2024-06-14 16:06:39 +0300

    feat add redis reset db

---
+++ src/redisReset.ts
@@ -0,0 +1,23 @@
+import { createClient } from 'redis';
+import { redisConfig } from './config';
+
+async function clearRedisDatabase() {
+  const client = createClient(redisConfig);
+
+  client.on('error', (err) => {
+    console.error('Redis Client Error', err);
+  });
+
+  await client.connect();
+
+  try {
+    await client.flushAll();
+    console.log('All keys and values have been deleted from the Redis database.');
+  } catch (err) {
+    console.error('Error while clearing the Redis database:', err);
+  } finally {
+    await client.disconnect();
+  }
+}
+
+clearRedisDatabase();
