6ae8ffb291ddbb7682d143f42c8b441da9b9aba5
berkingurcan <berkingurcan@yandex.com>
2024-10-15 16:57:24 +0700

    style remove space

--- src/lib/startAutoPosting.js
+++ src/lib/startAutoPosting.js
@@ -72,7 +72,6 @@ export const startAutoPosting = async (client, redisClient) => {
       } catch (error) {
         log.error(`Error deleting bot messages: ${error.message}`);
       }
-      
 
       for (const [i, toSend] of Object.entries(messagesToSend)) {
         if (i == 0) {
