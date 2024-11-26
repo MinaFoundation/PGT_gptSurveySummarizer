fb2882c5490741f5274df97733644e4f8bf221d5
berkingurcan <berkingurcan@yandex.com>
2024-11-26 19:28:58 +0300

    feat improve function

--- src/lib/getMembers.js
+++ src/lib/getMembers.js
@@ -1,14 +1,20 @@
 import log from "../logger.js";
 
-const getGuildMembers = async (guild) => {
+export const getGuildMembers = async (interaction) => {
     try {
+        const guild = interaction.guild;
+
         if (!guild) {
-            throw new Error('Guild not found.');
+            await interaction.followUp({ content: 'Guild not found.', ephemeral: true });
+            return [];
         }
 
         const members = await guild.members.fetch();
-        return members.map(member => ({ username: member.user.username, id: member.user.id }));
+        const memberList = members.map(member => ({ username: member.user.username, id: member.user.id }));
+
+        return memberList;
     } catch (e) {
         log.error(`An error occurred while fetching members: ${e}`);
+        await interaction.followUp({ content: `An error occurred: ${e.message}`, ephemeral: true });
     }
 };
