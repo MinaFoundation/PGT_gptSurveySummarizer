f50b408cf3de9d0cef3d5ec306ae638f51151a1a
berkingurcan <berkingurcan@yandex.com>
2024-12-13 14:28:12 +0300

    feat add Proposal model

--- src/models/govbotModel.ts
+++ src/models/govbotModel.ts
@@ -1,3 +1,10 @@
+export interface Proposal {
+    proposalName: string;
+    proposalDescription: string;
+    proposalAuthor: string;
+    lastEditTime: Date;
+}
+
 export interface Deliberation {
   proposalName: string;
   username: string;
